a03bc7783476e0af79370c51cbd46a1fab38d84a
KadirBalku <Kadir.Balku@gmail.com>
2024-04-02 20:04:21 +0200

    fix: baseurl when in vue application not url of server

--- src/HttpClient.js
+++ src/HttpClient.js
@@ -1,13 +1,13 @@
 class HTTPError extends Error {
-    constructor(code, statusText, message, response) {
-        super(message || statusText);
-        if (arguments.length >= 4 && response) {
-            Object.assign(this, response);
-        }
-        this.statusText = statusText;
-        this.statusCode = code;
-        this.response = response;
+  constructor(code, statusText, message, response) {
+    super(message || statusText);
+    if (arguments.length >= 4 && response) {
+      Object.assign(this, response);
     }
+    this.statusText = statusText;
+    this.statusCode = code;
+    this.response = response;
+  }
 }
 
 /**
@@ -21,56 +21,66 @@ class HTTPError extends Error {
  * @param {Object=} customConfig Some custom values for the fetch call.
  * @returns {Promise<Response>} Returns the raw request a promise, or throws a HTTPError.
  */
-function request(url, {json, method, params, ...customConfig} = {}) {
-    if (!method) {
-        method = json ? 'POST' : 'GET'
-    }
-    const config = {
-        method: method,
-        headers: {
-            'X-Requested-With': 'Fetch',
-            ...customConfig.headers,
-        },
-        ...customConfig,
-    }
-    if (json) {
-        config.body = JSON.stringify(json);
-        config.headers['Content-Type'] = 'application/json';
-    } else if (params && method === 'POST') {
-        if (typeof params === 'string' || params instanceof String) {
-            // provided a query string, e.g.: foo=1&bar=2
-            config.body = new URLSearchParams(params);
-        } else if (params instanceof HTMLFormElement) {
-            // provided a HTML form element
-            config.body = new FormData(params);
+
+function buildUrl(url) {
+  if (url && !(url.startsWith("http://") || url.startsWith("https://") || url.startsWith("//"))) {
+    url = (import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : window.location.origin) + url;
+  }
+  return url;
+}
+
+function request(url, { json, method, params, ...customConfig } = {}) {
+  if (!method) {
+    method = json ? "POST" : "GET";
+  }
+  const config = {
+    method: method,
+    headers: {
+      "X-Requested-With": "Fetch",
+      ...customConfig.headers,
+    },
+    ...customConfig,
+  };
+  if (json) {
+    config.body = JSON.stringify(json);
+    config.headers["Content-Type"] = "application/json";
+  } else if (params && method === "POST") {
+    if (typeof params === "string" || params instanceof String) {
+      // provided a query string, e.g.: foo=1&bar=2
+      config.body = new URLSearchParams(params);
+    } else if (params instanceof HTMLFormElement) {
+      // provided a HTML form element
+      config.body = new FormData(params);
+    } else {
+      // provided a object, e.g.: {foo: 1, bar: 2, baz: [3, 4]}
+      config.body = new FormData();
+      Object.entries(params).forEach(([key, value]) => {
+        if (Array.isArray(value)) {
+          for (const val of value) {
+            config.body.append(key, val);
+          }
         } else {
-            // provided a object, e.g.: {foo: 1, bar: 2, baz: [3, 4]}
-            config.body = new FormData();
-            Object.entries(params).forEach(([key, value]) => {
-                if (Array.isArray(value)) {
-                    for (const val of value) {
-                        config.body.append(key, val)
-                    }
-                } else {
-                    config.body.append(key, value)
-                }
-            });
+          config.body.append(key, value);
         }
+      });
     }
-    if (params && (method === 'GET' || json)) {
-        const getParams = new URLSearchParams(params)
-        url += `?${getParams.toString()}`;
+  }
+
+  if (params && (method === "GET" || json)) {
+    const getParams = new URLSearchParams(params);
+
+    url = `${buildUrl(url)}?${getParams.toString()}`;
+
+    //console.log("url for fetch", url);
+  }
+  return window.fetch(url, config).then(async (response) => {
+    if (response.ok) {
+      return response;
+    } else {
+      const errorMessage = `${response.status} ${response.statusText}: ${response.headers.get("x-viur-error")}`;
+      return Promise.reject(new HTTPError(response.status, response.statusText, errorMessage, response));
     }
-    return window
-        .fetch(url, config)
-        .then(async response => {
-            if (response.ok) {
-                return response
-            } else {
-                const errorMessage = `${response.status} ${response.statusText}: ${response.headers.get('x-viur-error')}`
-                return Promise.reject(new HTTPError(response.status, response.statusText, errorMessage, response))
-            }
-        })
+  });
 }
 
 /**
@@ -78,8 +88,7 @@ function request(url, {json, method, params, ...customConfig} = {}) {
  * @returns {Promise<String>} The resolved json-Promise (the skey).
  */
 function getSkey() {
-    return request('/json/skey')
-        .then(request => request.json())
+  return request("/json/skey").then((request) => request.json());
 }
 
 /**
@@ -87,17 +96,17 @@ function getSkey() {
  * @param {any} error The exception. Could be an instance of HTTPError or any other error/value.
  */
 function errorHandler(error) {
-    console.error(error);
-    let headline = `Error`;
-    if (error instanceof HTTPError) {
-        headline += ` ${error.statusCode} ${error.statusText}`;
-    }
-    alert(
-        `Sorry, an unexpected error occurred.
+  console.error(error);
+  let headline = `Error`;
+  if (error instanceof HTTPError) {
+    headline += ` ${error.statusCode} ${error.statusText}`;
+  }
+  alert(
+    `Sorry, an unexpected error occurred.
 ${headline}
 ${error}
-Please try again our contact our customer service.`,
-    )
+Please try again our contact our customer service.`
+  );
 }
 
-export {HTTPError, request, getSkey, errorHandler};
+export { HTTPError, request, getSkey, errorHandler };
